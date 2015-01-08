d35503b40ea4d22bb7d51650b99d3b964d14ae6c
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-01-08 13:11:55 -0200

    Changed Products component to show only current product when its defined.

--- src/js/components/products/products.jsx
+++ src/js/components/products/products.jsx
@@ -3,6 +3,7 @@ var React = require('react'),
     ProductStore = require('../../stores/ProductStore'),
     Masonry = require('../common/masonry.jsx'),
     ProductGrid = require('../product/gridView.jsx'),
+    ProductSingleView = require("../product/singleView.jsx"),
     ProductSingleDialog = require('../product/singleDialogView.jsx'),
     debug = require('debug')('products.jsx');
 
@@ -28,55 +29,65 @@ module.exports =
             ProductStore.addChangeListener(this._onChange);
         },
 
-        componentWillUnmount : function () {
+        componentWillUnmount: function () {
             ProductStore.removeChangeListener(this._onChange);
         },
 
-        render : function () {
-            var productGrid,
-                masonryOptions;
+        render: function () {
+            var content;
 
-            if (this.state.products.length > 0) {
-              productGrid = [];
-              var products = this.state.products.models;
-              var length = products.length;
-              for (var i = 0; i < length; i++) {
-                  var product = products[i];
-                  productGrid[i] = (
-                      <ProductGrid key={i} product={product} />
-                  );
-              }
+            if (this.state.currentProduct) {
+                content = (
+                    <ProductSingleView product={this.state.currentProduct} />
+                );
+
             } else {
-                productGrid = null;
-            }
+                var productGrid;
+                var masonryOptions = {
+                    transitionDuration: 0
+                };
+                if (this.state.products.length > 0) {
+                    productGrid = [];
+                    var products = this.state.products.models;
+                    var length = products.length;
+                    for (var i = 0; i < length; i++) {
+                        var product = products[i];
+                        productGrid[i] = (
+                            <ProductGrid key={i} product={product} />
+                        );
+                    }
+                } else {
+                    productGrid = null;
+                }
 
-            masonryOptions = {
-                transitionDuration: 0
-            };
+                // @todo quando o state mudar ele tentará limpar este elemento abaixo (React) e como não encontrará uma referência
+                // @todo a ele mais, pois o modal chamado futuramente se reposiciona no DOM, ele acusará um erro de INVARIANT
+                // @todo temos 3 possíveis soluções neste caso:
 
-            // @todo quando o state mudar ele tentará limpar este elemento abaixo (React) e como não encontrará uma referência
-            // @todo a ele mais, pois o modal chamado futuramente se reposiciona no DOM, ele acusará um erro de INVARIANT
-            // @todo temos 3 possíveis soluções neste caso:
+                // @todo  1. Pais ficam responsáveis por apenas inserir o elemento no DOM e nada mais (entretanto ainda assim terão uma referência
+                // @todo a este elemento, porém ela não será simplesmente mais utilizada (PROVAVELMENTE ERRADO)
 
-            // @todo  1. Pais ficam responsáveis por apenas inserir o elemento no DOM e nada mais (entretanto ainda assim terão uma referência
-            // @todo a este elemento, porém ela não será simplesmente mais utilizada (PROVAVELMENTE ERRADO)
+                // @todo 2. Todo elemento que se reposiciona no DOM não pode ser chamado por nenhum elemento pai, mas somente
+                // @todo ser embedado diretamente na página através do React.render(); (POSSIVEL SOLUÇÃO - parece um pouco mais engessada)
 
-            // @todo 2. Todo elemento que se reposiciona no DOM não pode ser chamado por nenhum elemento pai, mas somente
-            // @todo ser embedado diretamente na página através do React.render(); (POSSIVEL SOLUÇÃO - parece um pouco mais engessada)
+                // @todo 3. Somente statefull elements e que sejam TOP na hierarquia é que podem implementar elementos de
+                // @todo reposicionamento de DOM como o Tether Mixin, eles que devem criar tais filhos (POSSÍVEL SOLUÇÃO - parece um pouco mais flexível)
 
-            // @todo 3. Somente statefull elements e que sejam TOP na hierarquia é que podem implementar elementos de
-            // @todo reposicionamento de DOM como o Tether Mixin, eles que devem criar tais filhos (POSSÍVEL SOLUÇÃO - parece um pouco mais flexível)
+                // var modal = this.state.currentProduct ? <ProductSingleDialog openImmediately={true} product={this.state.currentProduct} /> : '';
 
-            var modal = this.state.currentProduct ? <ProductSingleDialog openImmediately={true} product={this.state.currentProduct} /> : '';
-
-            return (
-                <div className="products">
+                content = (
                     <Masonry options={masonryOptions}>
                         {productGrid}
                     </Masonry>
-                    {modal}
+                );
+            }
+
+            return (
+                <div className="products">
+                    {content}
                 </div>
             );
+
         },
 
         /**
