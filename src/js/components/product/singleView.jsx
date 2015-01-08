f6a41dd1d2cf6c2467cda84e2d750d5f4a4383c8
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-01-08 14:07:54 -0200

    Basic product layout.

--- src/js/components/product/singleView.jsx
+++ src/js/components/product/singleView.jsx
@@ -17,30 +17,46 @@ module.exports =
         },
 
         render : function () {
+            var product = this.props.product,
+                taxonomy = product.get('taxonomy');
 
-            var product = this.props.product;
+            var breadcrumb = taxonomy.map(function(item,index) {
+                var separator = (index > 0 && index < taxonomy.length - 1) ? '>' : '';
+                return (
+                    <div>
+                        <a>{item.type}-{item.id}</a>
+                        <span>{separator}</span>
+                    </div>
+                );
+            });
 
             return (
+                <div className="product-single-view">
 
-                <div>
-                    <h1 className="productName">{product.get('title')}</h1>
-                    <div layout="horizontal" layout-padding >
+                    <div className="product-header">
+                        <div className="product-title">
+                            <h1>{product.get('title')}</h1>
+                            <h2>{breadcrumb}</h2>
+                        </div>
+                        <div className="product-price">
+                        </div>
+                    </div>
 
-                        <div flex="33" layout="vertical" >
-                            <img className="productImg" src={product.get('image')} />
-                            <span flex id="productInfo"><span id="productInfoIcon"></span><p id="productInfoText">Mais informações</p></span>
-                            <span className="productTags">tags</span>
+                    <div className="product-body">
+
+                        <div className="product-image">
                         </div>
 
-                        <div flex="67">
-                            <div layout="horizontal" layout-align="start" >
-                                <div flex className="productPrices">
-                                    <p className="productPrice">por R$ {product.get('price')}</p>
-                                </div>
-                            </div>
-                            <div layout="horizontal" layout-align="start" >
-                                <AddProduct product={product} />
-                            </div>
+                        <div className="product-description">
+                        </div>
+
+                        <div className="product-offers">
+                        </div>
+
+                        <div className="product-price-history">
+                        </div>
+
+                        <div className="product-alert">
                         </div>
 
                     </div>
