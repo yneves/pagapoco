1909280e8e636369fe9d265521c8d27b24e92544
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-01-13 12:15:04 -0200

    Basic chart for price history.

--- src/js/components/product/priceHistory.jsx
+++ src/js/components/product/priceHistory.jsx
@@ -15,13 +15,41 @@ module.exports =
                 product: {}
             };
         },
+        
+        chartData: function() {
+            var product = this.props.product;
+            var history = product.get('price_history').y[1];
+            var data = {
+                labels: [],
+                series: [ [], [] ]
+            };
+            
+            history.forEach(function(item,index) {
+              data.labels[index] = item.day;
+              data.series[0][index] = item.min;
+              data.series[1][index] = item.max;
+            });
+            
+            return data;
+        },
+        
+        componentDidUpdate: function() {
+          var data = this.chartData();
+          var options = {
+            width: '300px',
+            height: '200px'
+          };
+          var elm = this.refs.chart.getDOMNode();
+          this.chartist = new Chartist.Line(elm, data, options);
+        },
 
         render: function () {
             var product = this.props.product;
             
             return (
                 <div className="price-history">
-
+                    <div className="price-history-chart" ref="chart">
+                    </div>
                 </div>
             );
         }
