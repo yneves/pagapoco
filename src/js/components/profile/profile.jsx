329a7f33d5889c520b7e7462946d30c3787fac91
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-01-13 13:20:22 -0200

    Fixed profile code style.

--- src/js/components/profile/profile.jsx
+++ src/js/components/profile/profile.jsx
@@ -4,15 +4,14 @@ var React = require('react'),
     Block = require('./block.jsx'),
     playerStore = require('../../stores/PlayerStore'),
     playerActions = require('../../actions/PlayerActionCreators'),
-    classSet = React.addons.classSet,
-    profile;
+    classSet = React.addons.classSet;
 
-profile =
+module.exports = 
     React.createClass({
 
         getInitialState: function () {
             return {
-                logged:  playerStore.getLogin()
+                logged: playerStore.getLogin()
             };
         },
 
@@ -28,13 +27,11 @@ profile =
             playerStore.removeChangeListener(this._onChange);
         },
 
-        handleSubmit: function(e) {
-            e.preventDefault();
-            var user = this.refs.username.getDOMNode().value.trim();
-            var pass = this.refs.password.getDOMNode().value.trim();
-            if (user && pass){
-                playerActions.logIn(user,pass);
-            }
+        handleSubmit: function(event) {
+            event.preventDefault();
+            var user = this.refs.username.getDOMNode().value;
+            var pass = this.refs.password.getDOMNode().value;
+            playerActions.logIn(user,pass);            
         },
 
         handleLogout: function(){
@@ -42,24 +39,29 @@ profile =
         },
 
         render: function () {
+            var content;
 
-
-            var display;
-            if (this.state.logged){
-                display = <div><h5>Logado</h5><a href="javascript:void(0)" onClick={this.handleLogout}>Deslogar</a></div>
+            if (this.state.logged) {
+                content = (
+                    <div>
+                        <h5>Logado</h5>
+                        <a href="javascript:void(0)" onClick={this.handleLogout}>Deslogar</a>
+                    </div>
+                );
+                
             } else {
-                display =   <form id="loginForm" onSubmit={this.handleSubmit}>
-<input type="text" ref="username" />
-                    <br />
-                    <input type="password" ref="password" />
-                    <br />
-                    <input type="submit" />
-                </form>;
-
+                content = (
+                    <form id="loginForm" onSubmit={this.handleSubmit}>
+                        <input type="text" ref="username" />
+                        <input type="password" ref="password" />
+                        <button type="submit"></button>
+                    </form>
+                );
             }
-            return(
+            
+            return (
                 <div>
-                {display}
+                    {display}
                 </div>
             );
         },
@@ -70,5 +72,3 @@ profile =
             );
         }
     });
-
-module.exports = profile;
