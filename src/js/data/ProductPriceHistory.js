75c657f41d8739199460bd899004a9278c91f8ce
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-01-22 16:39:57 -0200

    Fixed chart loop.

--- src/js/data/ProductPriceHistory.js
+++ src/js/data/ProductPriceHistory.js
@@ -20,49 +20,52 @@ ProductPriceHistoryModel = Model.extend({
     
     getChartData: function() {
       
-      var data = {
-          labels: [],
-          series: [[]]
-      };
+        var data = {
+            labels: [],
+            series: [[]]
+        };
       
-      function dateKey(time) {
-        var date = new Date(parseInt(time));        
-        var day = date.getDate();
-        var month = date.getMonth() + 1;
-        var year = date.getFullYear();
-        if (day < 10) day = "0" + day;
-        if (month < 10) month = "0" + month;
-        return year + "-" + month + "-" + day;
-      }
-      
-      var days = this.get('days');
-      var keys = Object.keys(days);
-      
-      var prices = {};
-      keys.forEach(function(day) {
-        prices[dateKey(day)] = parseFloat(days[day].min);
-      });
-      
-      var dates = [];
-      var first = Math.min.apply(null,keys);
-      var last = Math.max.apply(null,keys);
-      while (first <= last) {
-        dates.push(dateKey(first));
-        first += 24 * 60 * 60 * 1000;
-      }
-      
-      dates.forEach(function(date,index) {
-        var price = prices[date];
-        var prev = index - 1;
-        while (!price && prev >= 0) {
-          price = prices[ dates[prev] ];
-          prev--;
+        function dateKey(time) {
+            var date = new Date(parseInt(time));        
+            var day = date.getDate();
+            var month = date.getMonth() + 1;
+            var year = date.getFullYear();
+            if (day < 10) day = "0" + day;
+            if (month < 10) month = "0" + month;
+            return year + "-" + month + "-" + day;
         }
-        data.labels[index] = date;
-        data.series[0][index] = price;        
-      });
-              
-      return data;      
+      
+        var days = this.get('days');
+        var keys = Object.keys(days);
+
+        var prices = {};
+            keys.forEach(function(day) {
+            prices[dateKey(day)] = parseFloat(days[day].min);
+        });
+      
+        var dates = [];
+        if (keys.length) {
+            var first = Math.max(Math.min.apply(null,keys),1414807200000);
+            var last = new Date().getTime();
+            while (first <= last) {
+                dates.push(dateKey(first));
+                first += 24 * 60 * 60 * 1000;
+            }
+        } 
+      
+        dates.forEach(function(date,index) {
+            var price = prices[date];
+            var prev = index - 1;
+            while (!price && prev >= 0) {
+                console.log("prev");
+                price = prices[ dates[prev] ];
+                prev--;
+            }
+            data.labels[index] = date;
+            data.series[0][index] = price || 0;
+        });
+
+        return data;      
     },
     
 });
