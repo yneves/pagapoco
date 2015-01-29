46b19ffffe3dc4daf1ddef0ac1792df2533ec2a5
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-01-29 20:36:15 -0200

    Using seller logo.

--- src/js/components/product/storeList.jsx
+++ src/js/components/product/storeList.jsx
@@ -21,12 +21,15 @@ module.exports =
             var offersBySeller = this.props.product.get('offers_sellers');
             list = offersBySeller.map( function (sellerData) {
                 var price = sellerData.price.value.toFixed(2).replace(/\./,',');
-                var link = sellerData.links.length === 0 ? null : (
+                var link = sellerData.links.length ? (
                     <a href={sellerData.links[0].url} target="_blank">{Texts.storeList.view}</a>
-                );
+                ) : null;
+                var seller = sellerData.seller.thumbnail && sellerData.seller.thumbnail.url ? (
+                    <img src={sellerData.seller.thumbnail.url} />
+                ) : sellerData.seller.sellername;
                 return (
                     <li key={sellerData.id}>
-                        <span className="store-list-seller">{sellerData.seller.sellername}</span>
+                        <span className="store-list-seller">{seller}</span>
                         <strong className="store-list-price">{Texts.cs} {price}</strong>
                         <span className="store-list-parcel">{Texts.storeList.parcels.one}</span>
                         {link}
